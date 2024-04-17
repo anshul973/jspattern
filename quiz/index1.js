a5085941c410e1c6da722d5b97cb959b90e948cf
anshul973 <anshulraj973@gmail.com>
2024-04-17 14:46:57 +0530

    first commit

---
+++ quiz/index1.js
@@ -0,0 +1,52 @@
+
+
+const quizdata=[
+    {
+        question:"what is your name",
+        option:[
+            "anshul",
+            "raj",
+            "rahul",
+            "manis",
+        ],
+        correct:0,
+    },
+
+    {
+        question:"whait is your brother name",
+        option:[
+            "anshul",
+            "raj",
+            "vivek",
+            "manis",
+        ],
+        correct:2,
+        
+    },
+
+    {
+        question:"whait is your nick name",
+        option:[
+            "anshul",
+            "raj",
+            "vivek",
+            "manis",
+        ],
+        correct:1,
+        
+    },
+
+    {
+        question:"whait is your brother nick name",
+        option:[
+            "anshul",
+            "raj",
+            "vivek",
+            "manis",
+        ],
+        correct:1,
+        
+    }
+
+
+]
